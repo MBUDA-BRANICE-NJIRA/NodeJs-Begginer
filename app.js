c594093e79b738c9f5a0ac53bd4e1ef6f47ecec5
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-25 10:46:20 +0300

    Learnt on function expression

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ setInterval(function() {
     time += 2;//Tis adds time by 1 after given interval 
     console.log(time + 'seconds have passed');
     
-}, 2000);//This the interval time
+}, 2000);//This the interval time 
+
+
+//Normal function
+function sayHi() {
+    console.log('Hi');
+    
+}
+sayHi();
+
+//FUnction expression
+
+var sayBye = function() {
+    console.log("Bye");
+    
+};
+sayBye();
