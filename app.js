5d15299c2a745fce0833ab86b7689f2d28e43eee
MBUDA-BRANICE-NJIRA <mbudanjira@gmail.com>
2025-02-25 10:24:54 +0300

    Understood how timeInterval opperation

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ setTimeout(function () {
     console.log("3sec have passed");
     
 }, 3000);
+
+var time = 0;
+
+setInterval(function() {
+    time += 2;//Tis adds time by 1 after given interval 
+    console.log(time + 'seconds have passed');
+    
+}, 2000);//This the interval time
